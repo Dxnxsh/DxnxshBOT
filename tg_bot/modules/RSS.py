e05e2e2d5300789d80bd24ac7028954d8ee6b345
DxnxshFxrhxn <danishfarhan700@gmail.com>
2018-04-16 04:06:59 +0800

    update rss.py again

--- tg_bot/modules/RSS.py
+++ tg_bot/modules/RSS.py
@@ -1,224 +1,243 @@
+import html
 import re
-import logging
-import time
-import datetime
-import pickle
-import urllib
-import html.parser
-from os import path
+
+from feedparser import parse
+from telegram import ParseMode, constants
 from telegram.ext import CommandHandler
-from telegram.error import BadRequest
-import feedparser
-import tg_bot.plugins
-from tg_bot.snippets import stripln
 
-datadir = "data"
-log = logging.getLogger(__name__)
-NEWPOST = 'Новый пост в блоге <a href="{url}">{title}</a>!\nПост: {post}\n\n{content}'
+from tg_bot import dispatcher, updater
+from tg_bot.modules.helper_funcs.chat_status import user_admin
+from tg_bot.modules.sql import rss_sql as sql
 
-# Code for Tumblr special support
-valid_tags = re.compile(r'(<(/?)(b|i|pre|code|a href="[^"]+")>)')
-invalid_tags = re.compile(r'(?<!\\)[<][^>]*(?<!\\)[>]')
-paragraphy = re.compile('</?p>')
-unhr = re.compile('<hr>')
-unesq_right = re.compile('>')
-unesq_left = re.compile('<')
-validify_tags = re.compile(r'\\(<|<|>)')
 
-def nadekofy(string):
-    return html.parser.unescape(
-        validify_tags.sub(r'\1',
-            invalid_tags.sub('', 
-                paragraphy.sub('\n',
-                    unhr.sub('—————————', 
-                        valid_tags.sub(r'\<\2\3\>',
-                            unesq_left.sub('<', 
-                                unesq_right.sub('>', string)
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    ).strip()
+def show_url(bot, update, args):
+    tg_chat_id = str(update.effective_chat.id)
 
-def get_content(feed):
-    try:
-        if 'Tumblr' in feed['feed']['generator']:
-            # We can use <summary>, but we need to make HTML Nadeko-compatible.
-            return nadekofy(feed['entries'][0]['summary'])
+    if len(args) >= 1:
+        tg_feed_link = args[0]
+        link_processed = parse(tg_feed_link)
+
+        if link_processed.bozo == 0:
+            feed_title = link_processed.feed.get("title", default="Unknown")
+            feed_description = "<i>{}</i>".format(
+                re.sub('<[^<]+?>', '', link_processed.feed.get("description", default="Unknown")))
+            feed_link = link_processed.feed.get("link", default="Unknown")
+
+            feed_message = "<b>Feed Title:</b> \n{}" \
+                           "\n\n<b>Feed Description:</b> \n{}" \
+                           "\n\n<b>Feed Link:</b> \n{}".format(html.escape(feed_title),
+                                                               feed_description,
+                                                               html.escape(feed_link))
+
+            if len(link_processed.entries) >= 1:
+                entry_title = link_processed.entries[0].get("title", default="Unknown")
+                entry_description = "<i>{}</i>".format(
+                    re.sub('<[^<]+?>', '', link_processed.entries[0].get("description", default="Unknown")))
+                entry_link = link_processed.entries[0].get("link", default="Unknown")
+
+                entry_message = "\n\n<b>Entry Title:</b> \n{}" \
+                                "\n\n<b>Entry Description:</b> \n{}" \
+                                "\n\n<b>Entry Link:</b> \n{}".format(html.escape(entry_title),
+                                                                     entry_description,
+                                                                     html.escape(entry_link))
+                final_message = feed_message + entry_message
+
+                bot.send_message(chat_id=tg_chat_id, text=final_message, parse_mode=ParseMode.HTML)
+            else:
+                bot.send_message(chat_id=tg_chat_id, text=feed_message, parse_mode=ParseMode.HTML)
         else:
-            # Try to get the first Atom content value.
-            # for Nadeko-compatible feeds this will be the plain text.
-            return feed.entries[0].content[0].get(value) or ""
-        #else:
-        #    raise ValueError("Falling back to no content format.")
-    except:
-        # Fallback.
-        return ""
-
-class FeedSingleton(type):
-    _instances = {}
-
-    def __call__(cls, url):
-        if url not in cls._instances:
-            cls._instances[url] = super(FeedSingleton, cls).__call__(url)
-        return cls._instances[url]
+            update.effective_message.reply_text("This link is not an RSS Feed link")
+    else:
+        update.effective_message.reply_text("URL missing")
 
 
-class RSSFeed(metaclass=FeedSingleton):
-    def __init__(self, url):
-        self.url = url
-        self.subscribers = set()
-        self.updated = None
-        self.lastpost = None
+def list_urls(bot, update):
+    tg_chat_id = str(update.effective_chat.id)
 
-    def subscribe(self, uid):
-        self.subscribers.add(uid)
+    user_data = sql.get_urls(tg_chat_id)
 
-    def unsubscribe(self, uid):
-        self.subscribers.remove(uid)
+    # this loops gets every link from the DB based on the filter above and appends it to the list
+    links_list = [row.feed_link for row in user_data]
 
-    def check(self, bot, notify_status):
-        feed = feedparser.parse(self.url)
-        updated = datetime.datetime.fromtimestamp(time.mktime(
-            feed.get('updated_parsed') or feed['entries'][0]['published_parsed']
-        ))
-        if self.updated != updated:
-            # Feed is updated!
-            self.updated = updated
-            lastpost = feed['entries'][0]['id']
-            if lastpost != self.lastpost:
-                self.lastpost = lastpost
-                for chatid in self.subscribers:
-                    if isinstance(chatid, str) or notify_status.get('chatid') in [None, True]:
-                        try:
-                            msg = bot.send_message(
-                                chat_id=chatid,
-                                text=NEWPOST.format(
-                                    title=feed['feed']['title'],
-                                    url=feed['feed']['links'][0]['href'],
-                                    post=urllib.parse.unquote(
-                                        feed['entries'][0]['links'][0]['href']
-                                    ),
-                                    content=get_content(feed)
-                                ), parse_mode="HTML"
-                            )
-                        except:
-                            # Something went wrong, probably when getting content.
-                            # XXX Lock the exception on BadRequest from bad markup.
-                            msg = bot.send_message(
-                                chat_id=chatid,
-                                text=NEWPOST.format(
-                                    title=feed['feed']['title'],
-                                    url=feed['feed']['links'][0]['href'],
-                                    post=urllib.parse.unquote(
-                                        feed['entries'][0]['links'][0]['href']
-                                    ),
-                                    content="К сожалению, Телеграм ограничен в разметке, поэтому я не могу отправить содержимое поста. Пока что."
-                                ), parse_mode="HTML"
-                            )
-                        # If target is a channel name, lock onto the channel ID
-                        if isinstance(chatid, str):
-                            self.subscribers.remove(chatid)
-                            self.subscribers.add(msg.chat.id)
+    final_content = "\n\n".join(links_list)
 
-    def __getstate__(self):
-        return self.__dict__.copy()
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-
-    def __hash__(self):
-        return hash(self.url)
+    # check if the length of the message is too long to be posted in 1 chat bubble
+    if len(final_content) == 0:
+        bot.send_message(chat_id=tg_chat_id, text="This chat is not subscribed to any links")
+    elif len(final_content) <= constants.MAX_MESSAGE_LENGTH:
+        bot.send_message(chat_id=tg_chat_id, text="This chat is subscribed to the following links:\n" + final_content)
+    else:
+        bot.send_message(chat_id=tg_chat_id, parse_mode=ParseMode.HTML,
+                         text="<b>Warning:</b> The message is too long to be sent")
 
 
-class RSS(tg_bot.plugins.GenericPlugin):
-    def __init__(self, updater):
-        try:
-            with open(path.join(datadir, 'rss.dat'), 'rb') as f:
-                RSSFeed._instances.update(pickle.load(f))
-        except FileNotFoundError:
+@user_admin
+def add_url(bot, update, args):
+    if len(args) >= 1:
+        chat = update.effective_chat
+
+        tg_chat_id = str(update.effective_chat.id)
+
+        tg_feed_link = args[0]
+
+        link_processed = parse(tg_feed_link)
+
+        # check if link is a valid RSS Feed link
+        if link_processed.bozo == 0:
+            if len(link_processed.entries[0] >= 1):
+                tg_old_entry_link = link_processed.entries[0].link
+            else:
+                tg_old_entry_link = ""
+
+            # gather the row which contains exactly that telegram group ID and link for later comparison
+            row = sql.check_url_availability(tg_chat_id, tg_feed_link)
+
+            # check if there's an entry already added to DB by the same user in the same group with the same link
+            if row:
+                update.effective_message.reply_text("This URL has already been added")
+            else:
+                sql.add_url(tg_chat_id, tg_feed_link, tg_old_entry_link)
+
+                update.effective_message.reply_text("Added URL to subscription")
+        else:
+            update.effective_message.reply_text("This link is not an RSS Feed link")
+    else:
+        update.effective_message.reply_text("URL missing")
+
+
+@user_admin
+def remove_url(bot, update, args):
+    if len(args) >= 1:
+        tg_chat_id = str(update.effective_chat.id)
+
+        tg_feed_link = args[0]
+
+        link_processed = parse(tg_feed_link)
+
+        if link_processed.bozo == 0:
+            user_data = sql.check_url_availability(tg_chat_id, tg_feed_link)
+
+            if user_data:
+                sql.remove_url(tg_chat_id, tg_feed_link)
+
+                update.effective_message.reply_text("Removed URL from subscription")
+            else:
+                update.effective_message.reply_text("You haven't subscribed to this URL yet")
+        else:
+            update.effective_message.reply_text("This link is not an RSS Feed link")
+    else:
+        update.effective_message.reply_text("URL missing")
+
+
+def rss_update(bot, job):
+    user_data = sql.get_all()
+
+    # this loop checks for every row in the DB
+    for row in user_data:
+        row_id = row.id
+        tg_chat_id = row.chat_id
+        tg_feed_link = row.feed_link
+
+        feed_processed = parse(tg_feed_link)
+
+        tg_old_entry_link = row.old_entry_link
+
+        new_entry_links = []
+        new_entry_titles = []
+
+        # this loop checks for every entry from the RSS Feed link from the DB row
+        for entry in feed_processed.entries:
+            # check if there are any new updates to the RSS Feed from the old entry
+            if entry.link != tg_old_entry_link:
+                new_entry_links.append(entry.link)
+                new_entry_titles.append(entry.title)
+            else:
+                break
+
+        # check if there's any new entries queued from the last check
+        if new_entry_links:
+            sql.update_url(row_id, new_entry_links)
+        else:
             pass
-        self.jobqueue = updater.job_queue
-        self.updater = updater
-        self.dsp = updater.dispatcher
-        self.handlers = [
-            (CommandHandler(
-                'rss', self.reply, pass_job_queue=True,
-                pass_args=True, pass_chat_data=True
-            ), 0)
-        ]
-        self.feeds = set()
-        chat_data = self.dsp.chat_data
-        self.jobqueue.run_repeating(
-            self.check_feeds, interval=60, first=0,
-            context=lambda: {k: v.get('RSS_notify') for k, v in chat_data.items()}
-        )
 
-    def save_data(self):
-        log.debug("Saving persistent data...")
-        with open(path.join(datadir, 'rss.dat'), 'wb') as f:
-            pickle.dump(RSSFeed._instances, f)
-        log.debug("Done!")
+        if len(new_entry_links) < 5:
+            # this loop sends every new update to each user from each group based on the DB entries
+            for link, title in zip(reversed(new_entry_links), reversed(new_entry_titles)):
+                final_message = "<b>{}</b>\n\n{}".format(html.escape(title), html.escape(link))
 
-    def check_feeds(self, bot, job):
-        context = job.context()
-        feeds = RSSFeed._instances.values()
-        log.info("Checking updates for feeds...")
-        for feed in feeds:
-            log.info("Checking feed %s", feed.url)
-            try:
-                feed.check(bot, context)
-            except:
-                log.error("Error checking feed %s!", feed.url, exc_info=1)
-        self.save_data()
-
-    def reply(self, bot, update, args, chat_data, job_queue):
-        try:
-            cmd = args[0]
-        except IndexError:
-            update.message.reply_text(self.getHelp())
-            return
-        if cmd == "add":
-            feeds = map(RSSFeed, args[1:])
-            for feed in feeds:
-                feed.subscribe(update.message.chat_id)
-            update.message.reply_text("Добавила! Если найду новые посты, я сообщу! ^^")
-        elif cmd == "channel":
-            channelid = args[1]
-            userid = update.message.from_user.id
-            try:
-                if userid not in map(lambda m: m.user.id, bot.get_chat_administrators(channelid)):
-                    update.message.reply_text("Ты не являешься администратором этого канала.")
-                    return
-                feeds = map(RSSFeed, args[2:])
-                for feed in feeds:
-                    feed.subscribe(channelid)
-                update.message.reply_text("Добавила! Если найду что-то новенькое, оповещу твоих подписчиков!")
-            except BadRequest:
-                update.message.reply_text("Ой! Кажется, я не являюсь администратором этого канала.")
-        elif cmd == "notify":
-            try:
-                chat_data['RSS_notify'] = not chat_data['RSS_notify']
-            except KeyError:
-                chat_data['RSS_notify'] = False
-            update.message.reply_text("Уведомления {}.".format(
-                "включены" if chat_data['RSS_notify'] else "отключены"
-            ))
+                if len(final_message) <= constants.MAX_MESSAGE_LENGTH:
+                    bot.send_message(chat_id=tg_chat_id, text=final_message, parse_mode=ParseMode.HTML)
+                else:
+                    bot.send_message(chat_id=tg_chat_id, text="<b>Warning:</b> The message is too long to be sent",
+                                     parse_mode=ParseMode.HTML)
         else:
-            update.message.reply_text(self.getHelp())
+            for link, title in zip(reversed(new_entry_links[-5:]), reversed(new_entry_titles[-5:])):
+                final_message = "<b>{}</b>\n\n{}".format(html.escape(title), html.escape(link))
 
-    @staticmethod
-    def getHelp():
-        return stripln("""\
-            /rss - читалка для RSS.
-            /rss add <feed> - добавить новый фид по ссылке.
-            /rss channel <id> <feed> - публиковать обновления фида в канал.
-            Я должна быть в списке администраторов канала для публикации новых постов в канал.
-            Если канал публичный, вместо id можно подставить @короткоеимя канала.
-            Чтобы узнать ID приватного канала, воспользуйтесь @ChannelIDBot.
-            /rss notify - включить/отключить уведомления.
-            Об уведомлениях - каждые пять минут Нядэка проверяет \
-            RSS-ленты, добавленные в этом чате, и отправляет \
-            уведомления, если нашла что-то новенькое.""")
+                if len(final_message) <= constants.MAX_MESSAGE_LENGTH:
+                    bot.send_message(chat_id=tg_chat_id, text=final_message, parse_mode=ParseMode.HTML)
+                else:
+                    bot.send_message(chat_id=tg_chat_id, text="<b>Warning:</b> The message is too long to be sent",
+                                     parse_mode=ParseMode.HTML)
+
+            bot.send_message(chat_id=tg_chat_id, parse_mode=ParseMode.HTML,
+                             text="<b>Warning: </b>{} occurrences have been left out to prevent spam"
+                             .format(len(new_entry_links) - 5))
+
+
+def rss_set(bot, job):
+    user_data = sql.get_all()
+
+    # this loop checks for every row in the DB
+    for row in user_data:
+        row_id = row.id
+        tg_feed_link = row.feed_link
+        tg_old_entry_link = row.old_entry_link
+
+        feed_processed = parse(tg_feed_link)
+
+        new_entry_links = []
+        new_entry_titles = []
+
+        # this loop checks for every entry from the RSS Feed link from the DB row
+        for entry in feed_processed.entries:
+            # check if there are any new updates to the RSS Feed from the old entry
+            if entry.link != tg_old_entry_link:
+                new_entry_links.append(entry.link)
+                new_entry_titles.append(entry.title)
+            else:
+                break
+
+        # check if there's any new entries queued from the last check
+        if new_entry_links:
+            sql.update_url(row_id, new_entry_links)
+        else:
+            pass
+
+
+__help__ = """
+ - /addrss <link>: add an RSS link to the subscriptions.
+ - /removerss <link>: removes the RSS link from the subscriptions.
+ - /rss <link>: shows the link's data and the last entry, for testing purposes.
+ - /listrss: shows the list of rss feeds that the chat is currently subscribed to.
+NOTE: In groups, only admins can add/remove RSS links to the group's subscription
+"""
+
+__mod_name__ = "RSS Feed"
+
+job = updater.job_queue
+
+job_rss_set = job.run_once(rss_set, 5)
+job_rss_update = job.run_repeating(rss_update, interval=60, first=60)
+job_rss_set.enabled = True
+job_rss_update.enabled = True
+
+SHOW_URL_HANDLER = CommandHandler("rss", show_url, pass_args=True)
+ADD_URL_HANDLER = CommandHandler("addrss", add_url, pass_args=True)
+REMOVE_URL_HANDLER = CommandHandler("removerss", remove_url, pass_args=True)
+LIST_URLS_HANDLER = CommandHandler("listrss", list_urls)
+
+dispatcher.add_handler(SHOW_URL_HANDLER)
+dispatcher.add_handler(ADD_URL_HANDLER)
+dispatcher.add_handler(REMOVE_URL_HANDLER)
+dispatcher.add_handler(LIST_URLS_HANDLER)
