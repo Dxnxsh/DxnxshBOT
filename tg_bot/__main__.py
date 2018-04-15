2f683d6f7ba613d1beb4774c0490281c6c634ff9
DxnxshFxrhxn <danishfarhan700@gmail.com>
2018-04-15 13:49:55 +0800

    mainconfig2

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,13 +18,9 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! I'm a group manager bot maintained by [this wonderful person](tg://user?id={}).
+Hi {}, my name is {}! I'm a group manager bot maintained by [this wonderful person](t.me/Dxnxsh).
 I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick \
 [here](github.com/PaulSonOfLars/tgbot)!
-
-
-
-
 
 You can find the list of available commands with /help.
 
